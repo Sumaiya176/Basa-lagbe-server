649dae5224ae680845755fb99896f29ac0dc3b76
Sumaiya Talukder <sumaiya.tr96@gmail.com>
2025-08-20 00:21:47 +0600

    cors issue solved

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,12 @@ import mongoose from "mongoose";
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://basa-lagbe.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
